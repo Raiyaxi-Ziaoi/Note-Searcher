24ef72a1592665244bd65de78326b425e2c2637b
DadaLittleOne <82474669+DadaLittleOne@users.noreply.github.com>
2022-07-02 00:21:51 +0100

    First code added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Note-Searcher
- Searchers your notes for specific keywords
+
+A program that searches your notes for specific keywords made in Python made by Raiyaxi Ziaoi AKA DadaLittleOne
+
+Note: I am a beginner to Python so feel free to clone this repository to edit and optimise the code.
---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+from docx import Document
+
+
+def search(keyword):
+    file_list = []
+    for root, dirs, files in os.walk(os.getcwd()):
+        for file in files:
+            if os.path.join(root, file).endswith(".docx") or os.path.join(root, file).endswith(".txt"):
+                file_list.append(os.path.join(root, file))
+    for file in file_list:
+        if file.endswith(".docx"):
+            with open(file, "r", encoding='cp850') as f:
+                file_contents = f.read()
+                file_contents = file_contents.split("\n")
+                lineIndex = 1
+                doc = Document(file)
+                for para in doc.paragraphs:
+                    text = para.text
+                    text = text.split("\n")
+                    for line in text:
+                        if keyword in line:
+                            filePath = os.path.relpath(file, os.getcwd())
+                            print(
+                                f"\nKeyword: \"{keyword}\" File: \"{filePath}\" Line: \"{lineIndex}\"")
+                            lineIndex += 1
+                        else:
+                            lineIndex += 1
+                            break
+        else:
+            with open(file, "r", encoding='cp850') as f:
+                file_contents = f.read()
+                file_contents = file_contents.split("\n")
+                lineIndex = 1
+                for line in file_contents:
+                    if keyword in line:
+                        filePath = os.path.relpath(file, os.getcwd())
+                        print(
+                            f"\nKeyword: \"{keyword}\" File: \"{filePath}\" Line: \"{lineIndex}\"")
+                        lineIndex += 1
+                    else:
+                        lineIndex += 1
+                        break
+
+
+os.system('COLOR 0b')
+print("\nWelcome to Note Searcher")
+while True:
+    print("\nType exit to exit")
+    keyword = input("\nPlease enter a keyword to search for: ")
+    search(keyword)
+print("\nThank you for using Note Searcher")
+input("\nPress enter to exit...")
