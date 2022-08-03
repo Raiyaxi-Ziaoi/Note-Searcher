d9e5cf525bfde061d03287729d44edd32e360446
Raiyaxi-ZIaoi <82474669+Raiyaxi-Ziaoi@users.noreply.github.com>
2022-08-03 16:58:46 +0100

    REAL last commit

--- main.py
+++ main.py
@@ -4,58 +4,61 @@ import os
 from docx import Document
 
 
-def search(keyword): # Searches
+def search(keyword):  # Searches
     file_list = []
     found = 0
-    for root, dirs, files in os.walk(os.getcwd()): # Recursive directory search for files with .txt or .docx
+    # Recursive directory search for files with .txt or .docx
+    for root, dirs, files in os.walk(os.getcwd()):
         for file in files:
             if os.path.join(root, file).endswith(".docx") or os.path.join(root, file).endswith(".txt"):
                 file_list.append(os.path.join(root, file))
     for file in file_list:
-        if file.endswith(".docx"): # File ends in .docx?
-            with open(file, "r", encoding='cp850') as f: # Open file with weird encoding
+        if file.endswith(".docx"):  # File ends in .docx?
+            with open(file, "r", encoding='cp850') as f:  # Open file with weird encoding
                 file_contents = f.read()
                 file_contents = file_contents.split("\n")
                 lineIndex = 1
                 doc = Document(file)
-                for para in doc.paragraphs: # Reads .docx file
+                for para in doc.paragraphs:  # Reads .docx file
                     text = para.text
                     text = text.split("\n")
                     for line in text:
                         if keyword in line:
                             filePath = os.path.relpath(file, os.getcwd())
                             print(
-                                f"\nFile: \"{filePath}\" Line: \"{lineIndex}\"") # Prints out details
+                                f"\nFile: \"{filePath}\" Line: \"{lineIndex}\"")  # Prints out details
                             lineIndex += 1
                             found += 1
                         else:
                             lineIndex += 1
                             break
-        else: # Must be .txt file now
-            with open(file, "r", encoding='cp850') as f: # More encoding magic
+        else:  # Must be .txt file now
+            with open(file, "r", encoding='cp850') as f:  # More encoding magic
                 file_contents = f.read()
                 file_contents = file_contents.split("\n")
                 lineIndex = 1
                 for line in file_contents:
                     if keyword in line:
                         filePath = os.path.relpath(file, os.getcwd())
-                        print(f"\nFile: {filePath}, Line: {lineIndex}") # Prints out details
+                        # Prints out details
+                        print(f"\nFile: {filePath}, Line: {lineIndex}")
                         lineIndex += 1
                         found += 1
                     else:
                         lineIndex += 1
                         break
-    if found == 0: # Couldn't find keyword
+    if found == 0:  # Couldn't find keyword
         print("\nKeyword not found")
 
-if __name__ == "__main__": # Main
+
+if __name__ == "__main__":  # Main
     os.system('TITLE Note Searcher')
     os.system('COLOR 0b')
-    print("\nWelcome to Note Searcher")
+    print("Welcome to Note Searcher")
     while True:
-        print("\nType exit to exit")
+        print("\nType EXIT to exit the program.")
         keyword = input("\nPlease enter a keyword to search for: ")
-        if keyword.lower() == "exit":
+        if keyword == "EXIT":
             break
         search(keyword)
     print("\nThank you for using Note Searcher")
